ab8888d1c5e4a33075d53615f45d9f1478a997fc
qingyan520 <1627961573@qq.com>
2022-11-14 17:25:30 +0800

    服务注册功能完成

--- include/MrpcProvider.h
+++ include/MrpcProvider.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<google/protobuf/service.h>
+#include<google/protobuf/descriptor.h>
 #include<muduo/net/TcpServer.h>
 #include<muduo/net/TcpConnection.h>
 #include<muduo/net/EventLoop.h>
@@ -14,12 +15,20 @@
 class MrpcProvider{
     
     public:
+    //用来注册服务
     void NotifyService(google::protobuf::Service*service);
-
+    //启动一个rpc节点
     void Run();
     private:
-
+    //muduo库必备组件，进行事件循环，mainLoop负责新用户的连接事件
     muduo::net::EventLoop loop;
+
+    struct ServiceInfo{
+        google::protobuf::Service*service_;
+        std::unordered_map<std::string,const google::protobuf::MethodDescriptor*>MethodMap_;   //存储该服务对象所有的方法名称
+    };
+    //将每一个服务和其对应的名称存储到map表中，同时serveiceInfo中的map存储每一个服务对象的所有方法描述
+    std::unordered_map<std::string,ServiceInfo>ServiceMap_;
 
     //设置新用户连接或者断开时候的回调
     void OnConnection(const muduo::net::TcpConnectionPtr&);
--- src/MrpcProvider.cc
+++ src/MrpcProvider.cc
@@ -1,19 +1,37 @@
 #include"MrpcProvider.h"
 
 void MrpcProvider::NotifyService(google::protobuf::Service*service){
-        //得到每一个service提供者的名字，service对象，service里面的每一个方法名称和方法实现
-        //然后保存到map表格中，当调用方调用的时候，就可以直接用服务名称和方法名称进行调用了
+    //得到每一个service提供者的名字，service对象，service里面的每一个方法名称和方法实现
+    //然后保存到map表格中，当调用方调用的时候，就可以直接用服务名称和方法名称进行调用了
+
+    //得到一个对象方法的描述，这样我们就可以利用这个指针得到这个服务对象的详细信息
+    //比如对象名称，对象中每一个方法名称，方法
+    const google::protobuf::ServiceDescriptor*serviceDescriptor=service->GetDescriptor();
+    std::string service_name=serviceDescriptor->name();        
+                                //得到服务对象名称
+    ServiceInfo serviceInfo;
+    serviceInfo.service_=service;
+    //接下来得到所有方法集合
+    for(int i=0;i<serviceDescriptor->method_count();i++)
+    {
+        const google::protobuf::MethodDescriptor*methodDescriptor=serviceDescriptor->method(i);   //得到该服务里面每一个方法的描述
+        std::string method_name=methodDescriptor->name();                                         //得到方法名称
+        serviceInfo.MethodMap_.insert({method_name,methodDescriptor});                            //插入到map表中
+    }
+    ServiceMap_.insert({service_name,serviceInfo});
 }
 
 void MrpcProvider::Run(){
     //启动rpc服务器，设置读写回调，设置线程数量，进入事件循环
     //得到ip，port
     //这里后期进行改动，同时得到zookeeper的ip和地址，然后将服务注册到zookeeper上
+
     std::string ip=MrpcApplication::GetConfig().Load("server_ip");
     std::string port=MrpcApplication::GetConfig().Load("server_port");
     if(ip==""||port=="")
     {
         std::cout<<"GetConfig server_ip or server_port error!"<<std::endl;
+        exit(EXIT_FAILURE);
     }
     muduo::net::InetAddress addr(ip,atoi(port.c_str()));
     //建立服务器用来接受远程连接，并且设置端口可以复用
@@ -41,5 +59,5 @@ void MrpcProvider::OnConnection(const muduo::net::TcpConnectionPtr&conn)
 //设置消息处理回调
 void MrpcProvider::OnMessage(const muduo::net::TcpConnectionPtr&conn,muduo::net::Buffer*buf,muduo::Timestamp timestamp)
 {
-    
+
 }
